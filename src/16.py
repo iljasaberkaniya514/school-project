7e6e602c37c21a4788d1850cccc10e0fe76d03f9
iljasaberkaniya514 <iljasaberkaniya514@gmail.com>
2025-03-21 05:21:59 +0000

    Add 16.py

---
+++ src/16.py
@@ -0,0 +1,7 @@
+def calculate_area(length, width):
+    area = length * width
+    return area
+
+def calculate_volume(radius, height):
+    volume = 3.14 * (radius ** 2) * height
+    return volume
