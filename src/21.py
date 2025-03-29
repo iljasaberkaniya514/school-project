2499ac78670f783340367bea58bef5f35067ead6
iljasaberkaniya514 <iljasaberkaniya514@gmail.com>
2025-03-29 19:58:56 +0000

    Add 21.py

---
+++ src/21.py
@@ -0,0 +1,12 @@
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+# Example usage
+print(is_prime(7)) # Output: True
+print(is_prime(11)) # Output: True
+print(is_prime(49)) # Output: False
