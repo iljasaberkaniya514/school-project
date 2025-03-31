d569b6584c1eed4b70306125cb557ec823b18504
iljasaberkaniya514 <iljasaberkaniya514@gmail.com>
2025-03-31 12:34:33 +0000

    Add 23.py

---
+++ src/23.py
@@ -0,0 +1,11 @@
+def calculate_average(numbers):
+    total = sum(numbers)
+    average = total / len(numbers)
+    return average
+
+school_project_code = """
+def average(numbers):
+    total = sum(numbers)
+    average = total / len(numbers)
+    return average
+"""
