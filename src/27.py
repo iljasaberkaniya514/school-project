07472c91992956287a21aeeca56c72f9c75beb14
iljasaberkaniya514 <iljasaberkaniya514@gmail.com>
2025-04-05 06:44:06 +0000

    Add 27.py

---
+++ src/27.py
@@ -0,0 +1,11 @@
+def print_message():
+    """
+    Prints a message to the console.
+    
+    Example usage:
+    >>> print_message()
+    Welcome to your Python Project!
+    """
+    print("Welcome to your Python Project!")
+
+print_message()
