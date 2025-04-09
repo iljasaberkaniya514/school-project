7ae5ef34a843372e3bd37cb7e7a6a0f7cf48eda8
iljasaberkaniya514 <iljasaberkaniya514@gmail.com>
2025-04-09 09:11:25 +0000

    Add 31.py

---
+++ src/31.py
@@ -0,0 +1,8 @@
+def greet_user(name):
+    """
+    This function greets the user.
+    :param name: str - The name of the user to greet.
+    """
+    print(f"Hello, {name}!")
+
+greet_user("Alice")
